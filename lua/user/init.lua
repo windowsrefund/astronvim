c25f85bf50947dc22e26349a2028bd90087f741f
windowsrefund <mtf8>
2022-07-11 01:09:12 -0400

    improve cmp menu

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -66,6 +66,75 @@ local config = {
       ["stevearc/aerial.nvim"] = { disable = true }, -- this screws up keybindings like '{' and '}' and maybe folding too
     },
 
+    cmp = function(config)
+      local kind_icons = {
+        Text = " ",
+        Method = " ",
+        Function = " ",
+        Constructor = "",
+        Field = " ",
+        Variable = " ",
+        Class = " ",
+        Interface = " ",
+        Module = " ",
+        Property = " ",
+        Unit = " ",
+        Value = " ",
+        Enum = " ",
+        Keyword = " ",
+        Snippet = " ",
+        Color = " ",
+        File = " ",
+        Reference = " ",
+        Folder = " ",
+        EnumMember = " ",
+        Constant = " ",
+        Struct = " ",
+        Event = " ",
+        Operator = " ",
+        TypeParameter = " ",
+      }
+      local cmp = require "cmp"
+
+      return vim.tbl_deep_extend("force", config, {
+        experimental = {
+          ghost_text = true,
+        },
+        view = {
+          entries = { name = "custom", selection_order = "near_cursor" },
+        },
+        window = {
+          documentation = {
+            -- border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+            border = { " ", " ", " ", " ", " ", " ", " ", " " },
+          },
+        },
+        formatting = {
+          fields = { "abbr", "kind", "menu" },
+          format = function(entry, vim_item)
+            -- Kind icons
+            vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
+            -- Source
+            vim_item.menu = ({
+              buffer = "Buf",
+              nvim_lsp = "LSP",
+              luasnip = "LuaSnip",
+              nvim_lua = "Lua",
+              latex_symbols = "LaTeX",
+              cmp_tabnine = "Tabnine",
+              path = "Path",
+              emoji = "Emoji",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
+        mapping = {
+          ["<C-j>"] = cmp.config.disable,
+          ["<C-k>"] = cmp.config.disable,
+        },
+      })
+    end,
+
     bufferline = {
       options = {
         show_close_icon = false,
@@ -110,15 +179,6 @@ local config = {
       end
       return config
     end,
-  },
-
-  cmp = {
-    source_priority = {
-      nvim_lsp = 1000,
-      luasnip = 750,
-      buffer = 500,
-      path = 250,
-    },
   },
 
   ["which-key"] = {
