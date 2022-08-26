2ed4c92c0123e087be0aefe050782b143214d2e7
windowsrefund <mtf8>
2022-08-26 12:20:22 -0400

    claenup

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,21 +1,23 @@
 return {
 	opt = {
 		conceallevel = 2, -- enable conceal
-		foldcolumn = "2",
-		foldnestmax = 2,
+		foldcolumn = "3",
+		foldnestmax = 3,
 		foldenable = true,
 		foldlevel = 99,
 		foldlevelstart = 99,
-		-- foldmethod = "expr",
-		-- foldexpr = "nvim_treesitter#foldexpr()",
 		linebreak = true, -- linebreak soft wrap at words
 		list = true, -- show whitespace characters
-		listchars = { tab = "│→", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
-		-- fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]],
-		-- fillchars = "eob: ",
+		listchars = {
+			tab = "→ ",
+			extends = "⟩",
+			precedes = "⟨",
+			trail = "·",
+			nbsp = "␣",
+			eol = "↲",
+		},
 		shortmess = vim.opt.shortmess + { I = true },
-		-- showbreak = "↪ ",
-		showbreak = "╰─➤ ",
+		showbreak = "↪ ",
 		-- spellfile = "~/.config/astronvim/lua/user/spell/en.utf-8.add",
 		-- thesaurus = "~/.config/astronvim/lua/user/spell/thesaurus.txt",
 		wrap = true, -- soft wrap lines
