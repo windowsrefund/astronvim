29503bc1cd2be27d3c1591ce8ed6e426d8e65c26
windowsrefund <mtf8>
2022-09-05 01:35:42 -0400

    autorestart support

--- lua/user/polish.lua
+++ lua/user/polish.lua
@@ -1,8 +1,18 @@
 return function()
-  vim.filetype.add {
-    extension = {
-      tf = "terraform",
-      tfvars = "terraform-vars",
-    },
-  }
+	-- set autocommands
+	vim.api.nvim_create_augroup("packer_conf", { clear = true })
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		desc = "Sync packer after modifying plugins.lua",
+		group = "packer_conf",
+		pattern = "plugins.lua",
+		command = "source <afile> | PackerSync",
+	})
+
+	-- set custom filetypes
+	vim.filetype.add({
+		extension = {
+			tf = "terraform",
+			tfvars = "terraform-vars",
+		},
+	})
 end
